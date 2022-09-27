57f2a75f7f0146ce19abce87549e2577e6e9c2c0
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-09-27 15:01:25 +0300

    final changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom"
-//component file
-import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
